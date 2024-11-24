2a63101b9f771ef3d4f9d668dbf13ef828f24abe
Sim2K <sim2kuk@gmail.com>
2024-11-24 00:17:08 +0000

    Extracted SupaBase Securty keys into .env.local

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,12 +1,20 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://gpoymiyamqnfkpucxjcz.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdwb3ltaXlhbXFuZmtwdWN4amN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg2MTAyMzUsImV4cCI6MjA0NDE4NjIzNX0.W__VFHZhwY8pyeneTqCLNd5dDpy-cP2KKrIBhgZkMmo';
+if (!process.env.NEXT_PUBLIC_SUPABASE_URL) {
+  throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_URL');
+}
+if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
+  throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_ANON_KEY');
+}
 
-export const supabase = createClient(supabaseUrl, supabaseKey, {
-  auth: {
-    autoRefreshToken: true,
-    persistSession: true,
-    detectSessionInUrl: true
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  {
+    auth: {
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: true
+    }
   }
-});
+);
